e29a5844396dc1ce6b200d03fd8a8ab9e8bf1a08
James McGeachie <jamesmcgeachie10@gmail.com>
2015-08-23 18:29:07 -0400

    Completed edit and delete actions

--- controller.rb
+++ controller.rb
@@ -23,18 +23,37 @@ post "/create" do
     redirect "/"
 end
 
+# Create route to show one specific movie by ID
 get "/movies/:id" do
     @movie = Movie.find(params[:id].to_i)
     erb :show
 end
 
-# Create route to show one specific movie by ID
-
 # Create route to show form to edit one specific movie
+get "/movies/:id/edit" do
+    @movie = Movie.find(params[:id].to_i)
+    erb :edit
+end
 
 # Create route to update movie when edit form is submitted
+patch "/movies/:id" do
+    @movie = Movie.find(params[:id].to_i)
+    @movie.update({
+        title: params[:title],
+        director: params[:director],
+        rating: params[:rating]
+    })        
+    redirect "/"
+end
+
 
 # Create route to delete a movie 
+delete "/movies/:id" do
+    @movie = Movie.find(params[:id].to_i)
+    @movie.destroy
+    redirect "/"
+end
+
 
 
 
